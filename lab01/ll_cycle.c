40228953ab9dd980cb1979d59586bef8b5340bda
1318781143@qq.com <1318781143@qq.com>
2022-03-26 16:18:26 +0800

    2022_3_26_16_17 finish lap01 git add lab01!

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+	node* hare = head;
+	node* tortoise = head;
+
+	
+
+		while(hare != NULL){
+		
+		if(hare->next != NULL && hare->next->next != NULL)	
+
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+		if(hare == tortoise){
+			return 1;
+		}
+	}
+
+	
+	
     return 0;
 }
