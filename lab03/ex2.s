dd0a8acedd1e5b1d14b87c1c31047414c6709c2f
1318781143@qq.com <1318781143@qq.com>
2022-05-08 09:54:38 +0800

    lab05 finish with others answer

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -21,28 +21,29 @@ dest:
 
 .text
 main:
-    addi t0, x0, 0
-    addi s0, x0, 0
-    la s1, source
-    la s2, dest
+    addi t0, x0, 0  # t0 = 0
+    addi s0, x0, 0  # s0 = 0
+    la s1, source   # s1 = M(source)
+    la s2, dest     # s2 = M(dest)
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
-    addi sp, sp, -8
-    sw t0, 0(sp)
-    sw t2, 4(sp)
-    jal square
-    lw t0, 0(sp)
-    lw t2, 4(sp)
-    addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
-    add s0, s0, t2
-    addi t0, t0, 1
+    slli s3, t0, 2  # s3 = t0 * 4 s3
+    add t1, s1, s3  # t1 = s1 + s3
+    lw t2, 0(t1)    # t2 = M(t1 + 0)
+    beq t2, x0, exit    # if t2 == 0 jump exit
+    add a0, x0, t2      # a0 = t2 + 0
+
+    addi sp, sp, -8     # add sp(calling) 
+    sw t0, 0(sp)        # save t0
+    sw t2, 4(sp)        # save t2 --> source
+    jal square          # call square
+    lw t0, 0(sp)        # resume t0
+    lw t2, 4(sp)        # resume t2  t2 --> source
+    addi sp, sp, 8      # substract assignment space
+    add t2, x0, a0      # t2 = a0
+    add t3, s2, s3      # t3 = s3 + s2  -->comclude the address od dest[k]
+    sw t2, 0(t3)        # [t3] = t2  t3 --> dest[k]
+    add s0, s0, t2      # s0 = s0 + t2  ==> S0 --> sum
+    addi t0, t0, 1      # t0 = t0 + 1   ==> t0 --> k
     jal x0, loop
 square:
     add t0, a0, x0
